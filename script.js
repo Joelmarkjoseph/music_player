c2c35387fe023b2d2a6779f75223a964b5d71e05
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-06-04 04:49:03 +0530

    newwww

--- script.js
+++ script.js
@@ -1,47 +1,7 @@
-let c = 0;
-let song = document.getElementById("song");
-let pbar = document.getElementById("line");
-let btn = document.getElementById("ctrl");
 
-window.onload = function() {
-    // Get the URL parameter
-    const urlParams = new URLSearchParams(window.location.search);
-    const songFile = urlParams.get('song');
-    if (songFile) {
-        let as = document.getElementById('audiosrc');
-        as.src = songFile;
-        song.load();
-        pbar.max = song.duration;
-        pbar.value = song.currentTime;
-    }
-};
 
-pbar.onchange = function() {
-    song.currentTime = pbar.value;
-    if (song.paused) {
-        song.play();
-        btn.classList.remove('fa-play');
-        btn.classList.add('fa-pause');
-    }
-};
+function openplayer(sname){
+    var link='./player.html?song='+sname
+    location.replace(link);
 
-function playpause() {
-    if (c % 2 === 0) {
-        btn.classList.remove('fa-play');
-        btn.classList.add('fa-pause');
-        song.play();
-    } else {
-        btn.classList.remove('fa-pause');
-        btn.classList.add('fa-play');
-        song.pause();
-    }
-    c += 1;
-}
-
-song.addEventListener('timeupdate', () => {
-    pbar.value = song.currentTime;
-});
-
-function goback() {
-    location.replace('./index.html');
 }
--- scripts.js
+++ scripts.js
@@ -1,41 +1,47 @@
-let c=0;
+let c = 0;
 let song = document.getElementById("song");
-let pbar=document.getElementById("line");
-let btn=document.getElementById("ctrl");
+let pbar = document.getElementById("line");
+let btn = document.getElementById("ctrl");
 
-window.onload()=function(){
-    pbar.max=song.duration;
-    pbar.value=song.currentTime;
-    let as=document.getElementById('audiosrc');
-    as.src='Hillsong_Worship_-_Here_I_Am_To_Worship_(Jesusful.com).mp3';
-    song.onload();
-};
-pbar.onchange=function(){
-    song.currentTime=pbar.value;
-    if (song.paused()){
-        song.play();
-        btn.classList.remove('fa-play')
-        btn.classList.add('fa-pause')
+window.onload = function() {
+    // Get the URL parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const songFile = urlParams.get('song');
+    if (songFile) {
+        let as = document.getElementById('audiosrc');
+        as.src = songFile;
+        song.load();
+        pbar.max = song.duration;
+        pbar.value = song.currentTime;
     }
 };
-function playpause(){
-    if (c%2==0){
-        btn.classList.remove('fa-play')
-        btn.classList.add('fa-pause')
+
+pbar.onchange = function() {
+    song.currentTime = pbar.value;
+    if (song.paused) {
         song.play();
+        btn.classList.remove('fa-play');
+        btn.classList.add('fa-pause');
     }
-    else{
-        btn.classList.remove('fa-pause')
-        btn.classList.add('fa-play')
+};
+
+function playpause() {
+    if (c % 2 === 0) {
+        btn.classList.remove('fa-play');
+        btn.classList.add('fa-pause');
+        song.play();
+    } else {
+        btn.classList.remove('fa-pause');
+        btn.classList.add('fa-play');
         song.pause();
     }
-    c+=1
-};
+    c += 1;
+}
 
-song.addEventListener('timeupdate',() => {
-    pbar.value=song.currentTime;
+song.addEventListener('timeupdate', () => {
+    pbar.value = song.currentTime;
 });
-function goback(){
-    location.replace('./index.html');
 
+function goback() {
+    location.replace('./index.html');
 }
