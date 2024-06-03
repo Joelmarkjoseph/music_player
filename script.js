d6a054b3bd5cc6491dc4a3d586cf65f6933936fc
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-06-04 04:40:51 +0530

    wfsdfdsf

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-let c=0;
+let c = 0;
 let song = document.getElementById("song");
-let pbar=document.getElementById("line");
-let btn=document.getElementById("ctrl");
+let pbar = document.getElementById("line");
+let btn = document.getElementById("ctrl");
 
-function startsong(){
-    pbar.max=song.duration;
-    pbar.value=song.currentTime;
-};
-pbar.onchange=function(){
-    song.currentTime=pbar.value;
-    if (song.paused()){
-        song.play();
-        btn.classList.remove('fa-play')
-        btn.classList.add('fa-pause')
+window.onload = function() {
+    // Get the URL parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const songFile = urlParams.get('song');
+    if (songFile) {
+        let as = document.getElementById('audiosrc');
+        as.src = songFile;
+        song.load();
+        pbar.max = song.duration;
+        pbar.value = song.currentTime;
     }
 };
-function playpause(){
-    if (c%2==0){
-        btn.classList.remove('fa-play')
-        btn.classList.add('fa-pause')
+
+pbar.onchange = function() {
+    song.currentTime = pbar.value;
+    if (song.paused) {
         song.play();
+        btn.classList.remove('fa-play');
+        btn.classList.add('fa-pause');
     }
-    else{
-        btn.classList.remove('fa-pause')
-        btn.classList.add('fa-play')
+};
+
+function playpause() {
+    if (c % 2 === 0) {
+        btn.classList.remove('fa-play');
+        btn.classList.add('fa-pause');
+        song.play();
+    } else {
+        btn.classList.remove('fa-pause');
+        btn.classList.add('fa-play');
         song.pause();
     }
-    c+=1
-};
+    c += 1;
+}
 
-song.addEventListener('timeupdate',() => {
-    pbar.value=song.currentTime;
+song.addEventListener('timeupdate', () => {
+    pbar.value = song.currentTime;
 });
 
-function openplayer(sno,sname){
-    if (sno=='s1'){
-        location.replace('./player.html');
-        let as=document.getElementById('audiosrc');
-        as.src=sname;
-        song.onload();
-        startsong();
-    }
-}
-function goback(){
+function goback() {
     location.replace('./index.html');
-
 }
